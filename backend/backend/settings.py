6cc6528c3c17bd6839da747b30ce71604ffbdf3f
Gabriel Juvêncio da Silva <bieljuvencio@gmail.com>
2024-10-24 09:23:48 -0300

    gitignore e requirements

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'banco_crypto_insight',
-        'USER': 'postgres',            
-        'PASSWORD': '97944603',        
-        'HOST': 'localhost',           
-        'PORT': '5432',                
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
